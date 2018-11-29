e3ededf7ef8787877048eea0d36f954b48dda2c6
Joseph Huynh <joe.huynh@gmail.com>
2018-11-28 16:15:17 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render () {
+    return <div>Kitu Systems App</div>
+  }
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Include the Main React Dependencies
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('app'))
+
+// Enable hot reloading
+module.hot.accept()
